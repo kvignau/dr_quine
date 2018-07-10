6866091f15b06d4035ef618818e6e646bfd3d7e1
Kevin VIGNAU <kvignau@e2r6p16.42.fr>
2018-07-10 15:28:44 +0200

    Colleen asm

---
+++ ASM/Colleen.s
@@ -0,0 +1,27 @@
+section .data
+str:
+.string db "section .data%3$cstr:%3$c.string db %2$c%1$s%2$c%3$c%3$csection .text%3$cglobal start%3$cglobal _main%3$cextern _printf%3$c%3$cstart:%3$ccall _main%3$cret%3$c%3$c_main:%3$cpush rbp%3$cmov rbp, rsp%3$csub rsp, 16%3$clea rdi, [rel str.string]%3$clea rsi, [rel str.string]%3$cmov rdx, 34%3$cmov rcx, 10%3$ccall _printf%3$ccall _end%3$c%3$c_end:%3$cleave%3$cret%3$c"
+
+section .text
+global start
+global _main
+extern _printf
+
+start:
+call _main
+ret
+
+_main:
+push rbp
+mov rbp, rsp
+sub rsp, 16
+lea rdi, [rel str.string]
+lea rsi, [rel str.string]
+mov rdx, 34
+mov rcx, 10
+call _printf
+call _end
+
+_end:
+leave
+ret
