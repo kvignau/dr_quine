f7e34506cc915be60827793d15bfb23f7c87a9c8
Kevin VIGNAU <kvignau@e2r5p19.42.fr>
2018-07-11 12:17:58 +0200

    Grace 3 define

--- C/Grace.c
+++ C/Grace.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-#define no_main main
+#include <stdlib.h>
 
-void	ft_print(char *str)
-{
-	printf(str, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);
-}
+#define STR "#include <stdio.h>%1$c#define no_main main%1$c%1$cvoid	ft_print(char *str)%1$c{%1$cprintf(str, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);%1$c}%1$c%1$cint		no_main(void)%1$c{%1$cchar	*str;%1$c/*%1$c* com1%1$c*/%1$cstr = %1$c%2$c%3$s%2$c%1$c;%1$cft_print(str);%1$creturn (0);%1$c}%1$c"
+#define WRITE_FILE(file) fprintf(file, STR, 10, 34, STR)
+#define NO_MAIN() int main(void){FILE	*file = fopen("Grace_kid.c", "ab+"); /*com1*/if (!file){printf("Open file failure"); return (1);} if (WRITE_FILE(file) < 0){printf("Write file failure"); return(1);} fclose(file); return(0);}
 
-int		no_main(void)
-{
-	char	*str;
-	/*
-	* com1
-	*/
-	str = "#include <stdio.h>%c#define no_main main%c%cvoid	ft_print(char *str)%c{%c%cprintf(str, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);%c}%c%cint		no_main(void)%c{%c%cchar	*str;%c%c/*%c%c* com1%c%c*/%c%cstr = %c%s%c;%c%cft_print(str);%c%creturn (0);%c}%c";
-	ft_print(str);
-	return (0);
-}
+NO_MAIN()
+// int		ft_print(FILE *file)
+// {
+// 	return (fprintf(file, STR, 10, 34, STR));
+// }
+
+// int		no_main(void)
+// {
+// 	FILE	*file = fopen("Grace_kid.c", "ab+");
+// 	/*
+// 	* com1
+// 	*/
+// 	if (!file)
+// 	{
+// 		printf("Open file failure");
+// 		return (1);
+// 	}
+// 	if (ft_print(file) < 0)
+// 	{
+// 		printf("Write file failure");
+// 		return (1);
+// 	}
+// 	fclose(file);
+// 	return (0);
+// }
