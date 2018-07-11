7554f0405708dbadfb88d37bfdbce245bf63b3e2
Kevin VIGNAU <kvignau@e2r5p19.42.fr>
2018-07-11 19:53:15 +0200

    Grace.js

---
+++ JAVASCRIPT/Grace.js
@@ -0,0 +1,8 @@
+(function ft_print(){
+	const fs = require('fs');
+	fs.writeFile("Grace_kid.js", "(" + ft_print.toString() + ")()\n", function(err) {
+		if (err)
+			return console.log(err);
+	})
+	// com 2
+})()
