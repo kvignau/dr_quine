43637c45fa48ab67deeea414669892b8873fc627
Kevin VIGNAU <kvignau@e2r5p19.42.fr>
2018-07-11 20:03:09 +0200

    Sully ok

---
+++ JAVASCRIPT/Sully.js
@@ -0,0 +1,15 @@
+(function main(){
+	let i = 5;
+	ft_print(i);
+})()
+function ft_print(i){
+	const fs = require('fs');
+	while (i >= 0)
+	{
+		fs.writeFile("Sully_" + i + ".js", "(function main(){\n\tlet i = " + i + ";\n\tft_print(i);\n})()\n" + ft_print.toString() + "\n", function(err) {
+			if (err)
+				return console.log(err);
+		})
+		i--;
+	}
+}
