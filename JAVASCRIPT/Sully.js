0f2532107db2fb4fa6fd7c31cc8e1224c3b211e0
Kevin VIGNAU <kvignau@e3r4p20.42.fr>
2018-08-06 19:13:25 +0200

    ok

--- JAVASCRIPT/Sully.js
+++ JAVASCRIPT/Sully.js
@@ -1,16 +1,18 @@
-// TO DO : A REFAIRE !!
 (function main(){
 	let i = 5;
+	if (i <= 0)
+		return ;
 	ft_print(i);
 })()
 function ft_print(i){
+	const { exec } = require('child_process');
 	const fs = require('fs');
-	while (i >= 0)
-	{
-		fs.writeFile("Sully_" + i + ".js", "(function main(){\n\tlet i = " + i + ";\n\tft_print(i);\n})()\n" + ft_print.toString() + "\n", function(err) {
-			if (err)
-				return console.log(err);
-		});
+	if (fs.existsSync("Sully_" + i + ".js"))
 		i--;
-	}
+	fs.writeFile("Sully_" + i + ".js", "(function main(){\n\tlet i = " + i + ";\n\tif (i <= 0)\n\t\treturn ;\n\tft_print(i);\n})()\n" + ft_print.toString() + "\n", function(err) {
+		if (err)
+			return console.log(err);
+	});
+	const name = "Sully_" + i + ".js";
+	exec(`node ${name}`);
 }
